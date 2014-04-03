63fefc7dbfff5d0e3678afdd67862195734188d2
Iván Stepaniuk <istepaniuk@gmail.com>
2014-04-04 01:45:31 +0200

    bearing -> heading

--- test/rover/core_test.clj
+++ test/rover/core_test.clj
@@ -4,22 +4,22 @@
 
 (deftest the-mars-rover
   (testing "stays in the same position if it gets no commands"
-    (is (= {:x 0 :y 0 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "" []))))
+    (is (= {:x 0 :y 0 :heading :north} (move-rover {:x 0 :y 0 :heading :north} "" []))))
   (testing "moves forward"
-    (is (= {:x 0 :y 1 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "F" []))))
+    (is (= {:x 0 :y 1 :heading :north} (move-rover {:x 0 :y 0 :heading :north} "F" []))))
   (testing "moves backwards"
-    (is (= {:x 0 :y 1 :bearing :north} (move-rover {:x 0 :y 2 :bearing :north} "B" []))))
+    (is (= {:x 0 :y 1 :heading :north} (move-rover {:x 0 :y 2 :heading :north} "B" []))))
   (testing "moves forward twice"
-    (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FF" []))))
+    (is (= {:x 0 :y 2 :heading :north} (move-rover {:x 0 :y 0 :heading :north} "FF" []))))
   (testing "rotates right"
-    (is (= {:x 0 :y 0 :bearing :east} (move-rover {:x 0 :y 0 :bearing :north} "R" []))))
+    (is (= {:x 0 :y 0 :heading :east} (move-rover {:x 0 :y 0 :heading :north} "R" []))))
   (testing "rotates right twice"
-    (is (= {:x 0 :y 0 :bearing :south} (move-rover {:x 0 :y 0 :bearing :north} "RR" []))))
+    (is (= {:x 0 :y 0 :heading :south} (move-rover {:x 0 :y 0 :heading :north} "RR" []))))
   (testing "rotates left"
-    (is (= {:x 0 :y 0 :bearing :west} (move-rover {:x 0 :y 0 :bearing :north} "L"[]))))
+    (is (= {:x 0 :y 0 :heading :west} (move-rover {:x 0 :y 0 :heading :north} "L"[]))))
   (testing "moves forward while heading east"
-    (is (= {:x 1 :y 0 :bearing :east} (move-rover {:x 0 :y 0 :bearing :east} "F" []))))
+    (is (= {:x 1 :y 0 :heading :east} (move-rover {:x 0 :y 0 :heading :east} "F" []))))
   (testing "The position wraps arround 100 because the planet is round"
-    (is (= {:x 1 :y 99 :bearing :east} (move-rover {:x 99 :y 1 :bearing :south} "FFLFF" []))))
+    (is (= {:x 1 :y 99 :heading :east} (move-rover {:x 99 :y 1 :heading :south} "FFLFF" []))))
   (testing "stops moving when it encounters an obstacle"
-    (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FFFF" [{:x 0 :y 3}])))))
+    (is (= {:x 0 :y 2 :heading :north} (move-rover {:x 0 :y 0 :heading :north} "FFFF" [{:x 0 :y 3}])))))

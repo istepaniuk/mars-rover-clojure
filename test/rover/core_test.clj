cbe8aa1b21e3c990547446fb13cea1b6481161be
Ivan Stepaniuk <istepaniuk@vagrant2.ratichos>
2014-04-04 01:05:20 +0200

    refactor, test renames

--- test/rover/core_test.clj
+++ test/rover/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [rover.core :refer :all]))
 
+(def ^:private planet-size {:around-x 100 :around-y 100}
+  )
+
+(def ^:private forward-displacements {:north {:delta-x 0  :delta-y 1 }
+                                      :east  {:delta-x 1  :delta-y 0 }
+                                      :south {:delta-x 0  :delta-y -1}
+                                      :west  {:delta-x -1 :delta-y 0 }})
+
 (defn- calculate-rotation[command]
   (get {"R" 1 "L" -1} command 0)
   )
@@ -15,11 +23,6 @@
     )
   )
 
-(def ^:private forward-displacements {:north {:delta-x 0  :delta-y 1 }
-                                      :east  {:delta-x 1  :delta-y 0 }
-                                      :south {:delta-x 0  :delta-y -1}
-                                      :west  {:delta-x -1 :delta-y 0 }})
-
 (defn- backward-displacements [bearing]
   (let [{delta-x :delta-x delta-y :delta-y} (forward-displacements bearing)]
     {:delta-x (- delta-x) :delta-y (- delta-y)})
@@ -31,7 +34,8 @@
          } command {:delta-x 0 :delta-y 0})
   )
 
-(def ^:private planet-size {:around-x 100 :around-y 100}
+(defn- is-coordinate-an-obstacle? [coordinate obstacles]
+  (some #(= % coordinate) obstacles)
   )
 
 (defn- calculate-new-coordinates [position command]
@@ -43,36 +47,39 @@
     )
   )
 
-(defn- do-command [position command]
-  (let [{bearing :bearing} position]
-    (let [{new-x :x new-y :y} (calculate-new-coordinates position command)]
-        {:x new-x :y new-y :bearing (calculate-new-bearing bearing command)}
+(defn move-rover [initial-position commands obstacles]
+  (letfn [(do-command [position command]
+    (let [{x :x y :y bearing :bearing} position]
+      (let [{new-x :x new-y :y} (calculate-new-coordinates position command)]
+        (if (is-coordinate-an-obstacle? {:x new-x :y new-y} obstacles)
+          position
+          {:x new-x :y new-y :bearing (calculate-new-bearing bearing command)}
+          )
+        )
       )
+    )]
+    (reduce do-command initial-position (map str commands))
     )
   )
 
-(defn move-rover [position commands obstacles]
-  (reduce do-command position (map str commands))
-  )
-
-(deftest mars-rover-movements
-  (testing "The rover stays in the same position if it gets no commands"
+(deftest the-mars-rover
+  (testing "stays in the same position if it gets no commands"
     (is (= {:x 0 :y 0 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "" []))))
-  (testing "The rover moves forward"
+  (testing "moves forward"
     (is (= {:x 0 :y 1 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "F" []))))
-  (testing "The rover moves backwards"
+  (testing "moves backwards"
     (is (= {:x 0 :y 1 :bearing :north} (move-rover {:x 0 :y 2 :bearing :north} "B" []))))
-  (testing "The rover moves forward twice"
+  (testing "moves forward twice"
     (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FF" []))))
-  (testing "The rover rotates right"
+  (testing "rotates right"
     (is (= {:x 0 :y 0 :bearing :east} (move-rover {:x 0 :y 0 :bearing :north} "R" []))))
-  (testing "The rover rotates right twice"
+  (testing "rotates right twice"
     (is (= {:x 0 :y 0 :bearing :south} (move-rover {:x 0 :y 0 :bearing :north} "RR" []))))
-  (testing "The rover rotates left"
+  (testing "rotates left"
     (is (= {:x 0 :y 0 :bearing :west} (move-rover {:x 0 :y 0 :bearing :north} "L"[]))))
-  (testing "The rover moves forward while heading east"
+  (testing "moves forward while heading east"
     (is (= {:x 1 :y 0 :bearing :east} (move-rover {:x 0 :y 0 :bearing :east} "F" []))))
   (testing "The position wraps arround 100 because the planet is round"
     (is (= {:x 1 :y 99 :bearing :east} (move-rover {:x 99 :y 1 :bearing :south} "FFLFF" []))))
-  (testing "The rover stops when it encounters an obstacle"
-    (is (= {:x 0 :y 1 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FF" [{:x 0 :y 1}])))))
+  (testing "stops moving when it encounters an obstacle"
+    (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FFFF" [{:x 0 :y 3}])))))

39889ab5b0345f17807271ad69bfc9051e4cd620
Ivan Stepaniuk <istepaniuk@vagrant2.ratichos>
2014-04-03 21:37:33 +0200

    red, the rover turns right

--- test/rover/core_test.clj
+++ test/rover/core_test.clj
@@ -19,4 +19,6 @@
   (testing "The rover moves backwards"
     (is (= {:x 0 :y -1 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "B"))))
   (testing "The rover moves forward twice"
-    (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FF")))))
+    (is (= {:x 0 :y 2 :bearing :north} (move-rover {:x 0 :y 0 :bearing :north} "FF"))))
+  (testing "The rover rotates right"
+    (is (= {:x 0 :y 0 :bearing :east} (move-rover {:x 0 :y 0 :bearing :north} "R")))))
